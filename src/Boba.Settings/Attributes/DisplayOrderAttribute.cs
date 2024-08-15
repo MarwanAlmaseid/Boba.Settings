62901123b988e858ef8b4a29fcd68cfe4ac0decb
SyedSaqib Naqvi <ideal_snaqvi@dm.gov.ae>
2024-08-15 08:58:20 +0400

    added display order

---
+++ src/Boba.Settings/Attributes/DisplayOrderAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace Boba.Settings.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false)]
+public class BobaSettingsDisplayOrderAttribute : Attribute
+{
+    public int Order { get; }
+
+    public BobaSettingsDisplayOrderAttribute(int order)
+    {
+        Order = order;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property, Inherited = false)]
+public class BobaPropertyDisplayOrderAttribute : Attribute
+{
+    public int Order { get; }
+
+    public BobaPropertyDisplayOrderAttribute(int order)
+    {
+        Order = order;
+    }
+}
